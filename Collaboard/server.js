af40e401849106a89235f02b944f7c5223f14d93
Subhodip Ghosal <s35593106@gmail.com>
2025-10-30 15:07:26 +0000

    Added port in collaboard server file

--- Collaboard/server.js
+++ Collaboard/server.js
@@ -6,7 +6,7 @@ const { UserRouter } = require("./Routes/users");
 const { BoardRouter } = require("./Routes/boards");
 const { cardRouter } = require("./Routes/cards")
 const cookieParser = require('cookie-parser');
-
+const PORT = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.json());
@@ -17,8 +17,8 @@ app.use("/api/v1/cards", cardRouter)
 
 try {
     async function main() {
-        await mongoose.connect(`${process.env.MONGODB_URL}collaboard-db`);
-        app.listen(3000, () => {
+        await mongoose.connect(`${process.env.MONGODB_URL}${process.env.DB_NAME}`);
+        app.listen(PORT, () => {
             console.log("Running on port 3000")
         });
     }
